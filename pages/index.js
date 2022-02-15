8dd5377c0f62791a896094dfd5f60ec32feec3d3
vamsiduvvana <90998350+vamsiduvvana@users.noreply.github.com>
2022-02-15 18:44:37 +0530

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import Login from "../components/Login";
+import { useMoralis } from "react-moralis";
+import Header from "../components/Header";
+import Messages from "../components/Messages";
+
+export default function Home() {
+  const { isAuthenticated } = useMoralis();
+
+  if (!isAuthenticated) return <Login />;
+
+  return (
+    <div className="h-screen overflow-y-scroll bg-gradient-to-b from-black to-fuchsia-900 overflow-hidden">
+      <Head>
+        <title>Metaverse Chat</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="max-w-screen-2xl mx-auto">
+        {/* Header */}
+        <Header />
+
+        {/* Messages */}
+        <Messages />
+      </div>
+    </div>
+  );
+}
